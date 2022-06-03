51e0f14b174fbaa1e0bc3aef39a9801a493df8c4
Michał Hawełka <m.hawelka@gmail.com>
2022-06-03 19:37:04 +0200

    Unnecessary file cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { ThemeContext, ThemeContextType } from "../context/theme";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const { isFontLarge, setIsFontLarge, isMonochrome, setIsMonochrome } =
@@ -27,10 +25,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white dark:bg-gray-400">
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="bg-white dark:bg-gray-400 flex flex-col gap-4 h-screen">
         <button onClick={() => setIsMonochrome(!isMonochrome)}>
           Switch to dark mode
         </button>
@@ -39,51 +34,8 @@ const Home: NextPage = () => {
         </button>
         <button onClick={() => switchLanguage()}>Switch to polish</button>
 
-        <p className={styles.description}>{t("testText")}</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className="text-center">{t("testText")}</p>
       </main>
-
-      <footer className="bg-white dark:bg-gray-400">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
